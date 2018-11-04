543f89504fab0a963da7382a1121729962462205
João Carneiro <joao.carneiro@gmail.com>
2018-11-03 23:50:55 -0300

    HomeView finished

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+// Assets
+import Logo from '../assets/images/Logo_48.png';
+
 const Header = (props) => (
   <div onClick={() => props.history.push('/')}>
-    <h1>CURTO CIRCUITO</h1>
+    <h2>CURTO <img src={Logo} alt="Curto Circuito Logo"/>CIRCUITO</h2>
   </div>
 );
 
--- src/views/home-view.js
+++ src/views/home-view.js
@@ -12,6 +12,10 @@ import {
   DropdownMenu,
   DropdownItem
 } from 'reactstrap';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 
 // Component Style
 import '../style/home.scss';
@@ -24,7 +28,12 @@ export default class HomeView extends React.Component {
     this.toggleSplit = this.toggleSplit.bind(this);
     this.state = {
       dropdownOpen: false,
-      splitButtonOpen: false
+      splitButtonOpen: false,
+      origin: '',
+      destination: '',
+      originCoords: {},
+      destinationCoords: {},
+      walkingDistance: 200
     };
   }
   
@@ -43,14 +52,27 @@ export default class HomeView extends React.Component {
   getCurrentLocation = () => {
     this.setState({ gps: true }, () => {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(this.setCurrentLocation, this.showError);
+        navigator.geolocation.getCurrentPosition(this.setCurrentLocation, this.showError, { timeout:10000,  maximumAge: 0 });
       }
     });
   }
 
   setCurrentLocation = (position) => {
-    console.log(position);
-    this.setState({ gps: false })
+    /*global google */
+    let geocoder = new google.maps.Geocoder();
+    let location = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    let latlng = { location };
+
+      geocoder.geocode(latlng, (results, status) => {
+        if (status === 'OK') {
+          this.setState({ origin: results[0].formatted_address, gps: false, originCoords: location });
+        } else {
+          this.setState({ gps: false })
+        }
+      });
   }
 
   showError = (error) => {
@@ -72,8 +94,43 @@ export default class HomeView extends React.Component {
     }
 
     this.setState({ gps: false })
-}
-  
+  }
+
+  handleOriginChange = origin => {
+    this.setState({ origin });
+  };
+
+  handleOriginSelect = origin => {
+    geocodeByAddress(origin)
+      .then(results => {
+        this.setState({ origin })
+        return getLatLng(results[0])
+      })
+      .then(latLng => {
+        this.setState({ originCoords: latLng });
+      })
+      .catch(error => console.error('Error', error));
+  };
+
+  handleDestinationChange = destination => {
+    this.setState({ destination });
+  };
+
+  handleDestinationSelect = destination => {
+    geocodeByAddress(destination)
+      .then(results => {
+        this.setState({ destination })
+        return getLatLng(results[0])
+      })
+      .then(latLng => {
+        this.setState({ destinationCoords: latLng });
+      })
+      .catch(error => console.error('Error', error));
+  };
+
+  setWalkingDistance = (distance) => {
+    this.setState({ walkingDistance: distance });
+  }
   
   render() {
     return (
@@ -81,32 +138,98 @@ export default class HomeView extends React.Component {
         <Container>
           <FormGroup>
             <Label>Saindo de</Label>
-            <InputGroup>
-              <Input placeholder='Minha origem' />
-              <InputGroupAddon addonType="append">
-                <Button onClick={this.getCurrentLocation}>
-                { this.state.gps
-                ? <i className="fa fa-spinner fa-spin fa-lg" aria-hidden="true"/>
-                : <i className="fa fa-map-marker fa-lg" aria-hidden="true" />
-                }
-                </Button>
-              </InputGroupAddon>
-            </InputGroup>
+            <PlacesAutocomplete
+              value={this.state.origin}
+              onChange={this.handleOriginChange}
+              onSelect={this.handleOriginSelect}
+            >
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <React.Fragment>
+                  <InputGroup>
+                    <Input
+                      {...getInputProps({
+                        placeholder: 'Minha origem' 
+                      })}
+                    />
+                    <InputGroupAddon addonType="append">
+                      <Button onClick={this.getCurrentLocation}>
+                      { this.state.gps || loading
+                      ? <i className="fa fa-spinner fa-spin fa-lg" aria-hidden="true"/>
+                      : <i className="fa fa-map-marker fa-lg" aria-hidden="true" />
+                      }
+                      </Button>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  { suggestions.length > 0 &&
+                    <div className="autocomplete-dropdown-container">
+                      {suggestions.slice(0,2).map(suggestion => {
+                        const className = 'suggestion-item';
+                        return (
+                          <div
+                            {...getSuggestionItemProps(suggestion, {
+                              className
+                            })}
+                          >
+                            <span>{suggestion.description}</span>
+                            <hr />
+                          </div>
+                          
+                        );
+                      })}
+                    </div>
+                  }
+                </React.Fragment>
+              )}
+            </PlacesAutocomplete>
           </FormGroup>
           <FormGroup>
             <Label>Quero chegar a</Label>
-            <Input placeholder="Meu destino" />
+            <PlacesAutocomplete
+              value={this.state.destination}
+              onChange={this.handleDestinationChange}
+              onSelect={this.handleDestinationSelect}
+            >
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <React.Fragment>
+                  <InputGroup>
+                    <Input
+                      {...getInputProps({
+                        placeholder: 'Meu destino' 
+                      })}
+                    />
+                  </InputGroup>
+                  { suggestions.length > 0 &&
+                    <div className="autocomplete-dropdown-container">
+                      {suggestions.slice(0,2).map(suggestion => {
+                        const className = 'suggestion-item';
+                        return (
+                          <div
+                            {...getSuggestionItemProps(suggestion, {
+                              className
+                            })}
+                          >
+                            <span>{suggestion.description}</span>
+                            <hr />
+                          </div>
+                          
+                        );
+                      })}
+                    </div>
+                  }
+                </React.Fragment>
+              )}
+            </PlacesAutocomplete>
           </FormGroup>
           <FormGroup row>
             <Label>Andando no máximo</Label>
             <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.splitButtonOpen} toggle={this.toggleSplit}>
-              <Button outline>200 m</Button>
+              <Button onClick={this.toggleSplit} outline>{`${this.state.walkingDistance} m`}</Button>
               <DropdownToggle split outline />
               <DropdownMenu>
                 <DropdownItem header>Aceito andar até</DropdownItem>
-                <DropdownItem>100 metros</DropdownItem>
-                <DropdownItem>200 metros</DropdownItem>
-                <DropdownItem>500 metros</DropdownItem>
+                <DropdownItem onClick={() => this.setWalkingDistance(100)}>100 metros</DropdownItem>
+                <DropdownItem onClick={() => this.setWalkingDistance(200)}>200 metros</DropdownItem>
+                <DropdownItem onClick={() => this.setWalkingDistance(500)}>500 metros</DropdownItem>
               </DropdownMenu>
             </InputGroupButtonDropdown>
           </FormGroup>
